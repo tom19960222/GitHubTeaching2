1286e4bb82593eeff556e31c0f38f9688f3cbf17
爆頭專家 <tom19960222@gmail.com>
2015-05-19 23:09:04 +0800

    empty tab frame

---
+++ src/net/hsexpert/main/ShapeTest.java
@@ -0,0 +1,19 @@
+package net.hsexpert.main;
+
+import net.hsexpert.main.TabFrame;
+
+import javax.swing.*;
+
+/**
+ * Created by ikaros on 2015/3/17.
+ */
+public class ShapeTest {
+    public static void main(String[] args)
+    {
+        TabFrame app = new TabFrame();
+        app.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        app.setSize(1280, 720);
+        app.setVisible(true);
+    }
+
+}
---
+++ src/net/hsexpert/main/TabFrame.java
@@ -0,0 +1,18 @@
+package net.hsexpert.main;
+
+import javax.swing.*;
+
+/**
+ * Created by Ikaros on 2015/4/20.
+ */
+
+public class TabFrame extends JFrame {
+
+    JTabbedPane tabPane;
+
+    public TabFrame() {
+        tabPane = new JTabbedPane();
+        this.add(tabPane);
+    }
+
+}
